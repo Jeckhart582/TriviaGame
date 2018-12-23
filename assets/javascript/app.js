6d8fcfbb88642954e7c28b25daf94e595d50c609
jeckhart582@gmail.com <jeckhart582@gmail.com>
2018-12-22 19:11:17 -0600

    Another push, rewrote js to pick questions randomly

---
+++ assets/javascript/app.js
@@ -0,0 +1,72 @@
+// Variables I can assign for correct answers
+// Variables I can assign for incorrect answers
+// Build a timers function that counts down
+// If/ Else statesments to determine whether a questions answer is correct
+// Fuction that runs if/else statment when submit button is pressed
+// Seperate page for listing what questions the user missed and how many they got right
+
+// var correctAnswer1 = "Zeus"
+// var incorrectAnswer1 = ["Hades", "Odin", "Athena"]
+
+// function question1 () {
+//     for ( var i = 0; i < incorrectAnswer1.length; i++) {
+//         var answer = $(".form-check-label");
+//         answer.addClass("answer");
+//         $(".form-check-label").html(incorrectAnswer1[i]);
+
+
+//     }
+// };
+
+
+
+// question1();
+$(document).ready(function () {
+    var choice = [
+        {
+            question: "Who is called the 'All-father' in Norse Mythology?",
+            questionOptions: ["Thor", "Hela", "Loki", "Zeus", "Odin"],
+            questionAnswer: 4
+        },
+        {
+            question: "Which Greek God was conisdered the god of war?",
+            questionOptions: ["Athena", "Hades", "Ares", "Posiden", "Hercules"],
+            questionAnswer: 2
+        },
+        {
+            question: "This Goddess was is know for granting bountiful harvests and plenty of grains?",
+            questionOptions: ["Hepastus", "Athena", "Demeter", "Aphrodite", "Hera"],
+            questionAnswer: 2
+        }
+
+    ];
+
+    function questionFunction() {
+        index = Math.floor(Math.random() * choice.length);
+        pick = choice[index];
+        $(".question").html("<h2>" + pick.question + "</h2>");
+        for (var i = 0; i < pick.questionOptions.length; i++) {
+            var userGuess = $("<button>");
+            userGuess.addClass("answerchoice");
+            userGuess.html(pick.questionOptions[i]);
+            userGuess.attr("data-guess", i);
+            $(".answers1").append(userGuess)
+        }
+
+    }
+
+    // function questionFunction2() {
+    //     $(".question2").html(question.question2)
+    //     for (i = 0; i < question.question2Answers.length; i++) {
+    //         var answerBox = $("<button>")
+    //         answerBox.addClass("letter-button letter letter-button-color");
+    //         answerBox.text(question.question2Answers[i]);
+    //         $(".answers2").append(answerBox);
+    //     }
+    // }
+
+    questionFunction();
+
+
+});
+
