de90f684a33ed8d906dfa310dcfdebf9fbcc0503
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-05-07 20:30:42 -0500

    Finshed assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,125 +1,174 @@
-// Variables I can assign for correct answers
-// Variables I can assign for incorrect answers
-// Build a timers function that counts down
-// If/ Else statesments to determine whether a questions answer is correct
-// Fuction that runs if/else statment when submit button is pressed
-// Seperate page for listing what questions the user missed and how many they got right
+$(document).ready(function(){
 
-// var correctAnswer1 = "Zeus"
-// var incorrectAnswer1 = ["Hades", "Odin", "Athena"]
-
-// function question1 () {
-//     for ( var i = 0; i < incorrectAnswer1.length; i++) {
-//         var answer = $(".form-check-label");
-//         answer.addClass("answer");
-//         $(".form-check-label").html(incorrectAnswer1[i]);
-
-
-//     }
-// };
-
-
-
-// question1();
-$(document).ready(function () {
-    var choice = [
-        {
-            question: "Who is called the 'All-father' in Norse Mythology?",
-            questionOptions: ["Thor", "Hela", "Loki", "Zeus", "Odin"],
-            questionAnswer: 4
-        },
-        {
-            question: "Which Greek God was conisdered the god of war?",
-            questionOptions: ["Athena", "Hades", "Ares", "Posiden", "Hercules"],
-            questionAnswer: 2
-        },
-        {
-            question: "Which Goddess was is know for granting bountiful harvests and plenty of grains?",
-            questionOptions: ["Hepastus", "Athena", "Demeter", "Aphrodite", "Hera"],
-            questionAnswer: 2
-        },
-        {
-            question: "Which mythological creature was a combination of a lion, snake and eagle?",
-            questionOptions: ["Chimera", "Griffin", "Hippogriff", "Hydra", "Satyr"],
-            questionAnswer: 0
-        },
-        {
-            question: "This creature is known in Norse Mythology as the 'World-Serpent'",
-            questionOptions: ["Jormungandr", "Jotunn", "Kraken", "Troll", "Warg"],
-            questionAnswer: 0
-        }
-
-
-    ];
-
-    var timer = 90;
-    var running = false;
-    var pick;
-    var index;
-    var correctAnswers = 0;
-    var incorrectAnswer = 0;
-
-    function decrement() {
-        $("#timerDown").html(timer);
-        timer--;
-
-        if (timer === 0) {
-            stop();
-        }
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+  })
+  
+  var trivia = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : '',
+    questions: {
+      q1: 'Who is the Greek God of War',
+      q2: 'Which Norse God is known for having two faces?',
+      q3: 'This Egyptian God has a head of a dog',
+      q4: '',
+      q5: "",
+      q6: '',
+      q7: ""
+    },
+    options: {
+      q1: ['Ares', 'Zeus', 'Hephastus', 'Steve'],
+      q2: ['Odin', 'Thor', 'Hela', 'Loki'],
+      q3: ['Set', 'Anubis', 'Faye', ''],
+      q4: ['3', '8', '11', '6'],
+      q5: ['Rachel','Phoebe','Emily','Carol'],
+      q6: ['Joey','Chandler','Rachel','Ross'],
+      q7: ['Ross', 'Phoebe', 'Monica','Chandler']
+    },
+    answers: {
+      q1: 'Ares',
+      q2: 'Hela',
+      q3: 'Anubis',
+      q4: '11',
+      q5: 'Rachel',
+      q6: 'Chandler',
+      q7: 'Phoebe'
+    },
+    startGame: function(){
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      // show game section
+      $('#game').show();
+      
+      //  empty last results
+      $('#results').html('');
+      
+      // show timer
+      $('#timer').text(trivia.timer);
+      
+      // remove start button
+      $('#start').hide();
+  
+      $('#remaining-time').show();
+      
+      // ask first question
+      trivia.nextQuestion();
+      
+    },
+    // method to loop through and display questions and options 
+    nextQuestion : function(){
+      
+      // set timer to 20 seconds each question
+      trivia.timer = 10;
+       $('#timer').removeClass('last-seconds');
+      $('#timer').text(trivia.timer);
+      
+      // to prevent timer speed up
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      // gets all the questions then indexes the current questions
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $('#question').text(questionContent);
+      
+      // an array of all the user options for the current question
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+      // creates all the trivia guess options in the html
+      $.each(questionOptions, function(index, key){
+        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+      })
+      
+    },
+    // method to decrement counter and count unanswered if timer runs out
+    timerRunning : function(){
+      // if timer still has time left and there are still questions left to ask
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $('#timer').text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $('#timer').addClass('last-seconds');
+          }
+      }
+      // the time has run out and increment unanswered, run result
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      }
+      // if all the questions have been shown end the game, show results
+      else if(trivia.currentSet === Object.keys(trivia.questions).length){
+        
+        // adds results of game (correct, incorrect, unanswered) to the page
+        $('#results')
+          .html('<h3>Thank you for playing!</h3>'+
+          '<p>Correct: '+ trivia.correct +'</p>'+
+          '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+          '<p>Please play again!</p>');
+        
+        // hide game sction
+        $('#game').hide();
+        
+        // show start button to begin a new game
+        $('#start').show();
+      }
+      
+    },
+    // method to evaluate the option clicked
+    guessChecker : function() {
+      
+      var resultId;
+      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+      if($(this).text() === currentAnswer){
+        // turn button green for correct
+        $(this).addClass('btn-success').removeClass('btn-info');
+        
+        trivia.correct++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Correct Answer!</h3>');
+      }
+      // else the user picked the wrong option, increment incorrect
+      else{
+        // turn button clicked red for incorrect
+        $(this).addClass('btn-danger').removeClass('btn-info');
+        
+        trivia.incorrect++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+      }
+      
+    },
+    // method to remove previous question results and options
+    guessResult : function(){
+      
+      // increment to next question set
+      trivia.currentSet++;
+      
+      // remove the options and results
+      $('.option').remove();
+      $('#results h3').remove();
+      
+      // begin next question
+      trivia.nextQuestion();
+       
     }
-
-    function timerRun() {
-        if (!running) {
-            intervalId = setInterval(decrement, 1000);
-            running = true;
-        }
-    };
-
-    function questionFunction() {
-        index = Math.floor(Math.random() * choice.length);
-        pick = choice[index];
-        $(".question").html("<h2>" + pick.question + "</h2>");
-        for (var i = 0; i < pick.questionOptions.length; i++) {
-            var userGuess = $("<button>");
-            userGuess.addClass("answerChoice");
-            userGuess.html(pick.questionOptions[i]);
-            userGuess.attr("data-guess", i);
-            $(".answers1").append(userGuess)
-        }
-
-    }
-
-    function questionFunction2() {
-        index = Math.floor(Math.random() * choice.length);
-        pick = choice[index];
-        $(".question2").html("<h2>" + pick.question + "</h2>");
-        for (var i = 0; i < pick.questionOptions.length; i++) {
-            var userGuess = $("<button>");
-            userGuess.addClass("answerChoice");
-            userGuess.html(pick.questionOptions[i]);
-            userGuess.attr("data-guess", i);
-            $(".answers2").append(userGuess)
-        }
-
-    }
-
-    questionFunction();
-    questionFunction2();
-
-    // $(".answerChoice").on("click", function () {
-    //     userGuess = parseInt($(this).attr("data-guess"));
-    //     console.log(userGuess)
-    //     if (userGuess === pick.questionAnswer) {
-    //         correctAnswers++;
-    //         console.log(correctAnswers)
-
-    //     }
-    // }
-    // );
-
-    timerRun();
-    decrement();
-
-
-});
-
+  
+  }
